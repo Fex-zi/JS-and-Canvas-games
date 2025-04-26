fa20ce8ccb769ab9b3009aba2088b59b91533e5a
Fexzi <fexzi87@gmail.com>
2025-04-26 12:15:42 -0600

    updates-enter-key

---
+++ dice/js/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+//Selecting Elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.querySelector('#score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//starting condition
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+  diceEL.classList.add('hidden');
+  player0EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+  player0EL.classList.add('player--active');
+  player1EL.classList.remove('player--active');
+};
+init();
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0EL.classList.toggle('player--active');
+  player1EL.classList.toggle('player--active');
+};
+
+//rolling dice function
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEL.classList.remove('hidden');
+    diceEL.src = `img/dice-${dice}.png`;
+
+    if (dice !== 1) {
+      //add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+//store score
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //Add current score to active player
+    scores[activePlayer] += currentScore; //scores[1] or[2]
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //check if player's score is >=30 to win for fasteR testing
+    if (scores[activePlayer] >= 30) {
+      //finish the game
+      playing = false;
+      diceEL.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
--- number-guess/js/script.js
+++ number-guess/js/script.js
@@ -13,50 +13,63 @@
 // console.log((document.querySelector('.guess').value = 23));
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
+let won = false;
 let score = 20; //state variable
 let highscore = 0;
+
 //just made this funcTion guy to reduce my code repetition--
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+const init = function () {
+  if (!won) {
+    const guess = Number(document.querySelector('.guess').value);
+    //console.log(guess);
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  //console.log(guess);
+    if (!guess) {
+      displayMessage('⛔ No number!');
 
-  if (!guess) {
-    displayMessage('⛔ No number!');
-
-    //won
-  } else if (guess === secretNumber) {
-    // document.querySelector('.message').textContent =
-    //   '🍷 Correct Number, You won!';
-    displayMessage('🍷 Correct Number, You won!');
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.background = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.highscore').textContent = score;
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      //won
+    } else if (guess === secretNumber) {
+      // document.querySelector('.message').textContent =
+      //   '🍷 Correct Number, You won!';
+      won = true;
+      displayMessage('🍷 Correct Number, You won!');
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('body').style.background = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.highscore').textContent = score;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    }
+    //when guess is wrong
+    else if (guess !== secretNumber) {
+      if (score > 1) {
+        displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
+        score--; //or score = score -1;
+        document.querySelector('.score').textContent = score;
+      } else {
+        // document.querySelector('.message').textContent = '😂 You Lost the game';
+        displayMessage('😂 You Lost the game');
+        document.querySelector('.score').textContent = 0;
+      }
     }
   }
-  //when guess is wrong
-  else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
-      score--; //or score = score -1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      // document.querySelector('.message').textContent = '😂 You Lost the game';
-      displayMessage('😂 You Lost the game');
-      document.querySelector('.score').textContent = 0;
-    }
+};
+
+document.querySelector('.check').addEventListener('click', init);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Enter') {
+    init();
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
+  won = false;
   score = 20;
   document.querySelector('.score').textContent = score;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
